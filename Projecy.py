adb2ea7e443d68cdde0cc4a53572dbbbeac34104
minhnhat416 <nhatnhm20416c@st.uel.edu.vn>
2023-01-31 15:21:13 +0700

    Outline

---
+++ Projecy.py
@@ -0,0 +1,15 @@
+# I Data Preparation
+# 1. Load Time Series Data
+
+# 2.  Exploring Time Series Data
+
+# II. Feature Engineering
+# 1, Feature Engineering for Time Series
+
+# III. Visualization
+# 1. Line plots
+# 2. Histograms and Density Plots
+# 3. Box and Whisker Plots
+# 4. Heat Maps
+# 5. Lag Plots or Scatter Plots
+# 6. Autocorrelation Plots.
