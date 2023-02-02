afe0599b14ecd9b4856efce23fb0b55d91d8cc07
minhnhat416 <nhatnhm20416c@st.uel.edu.vn>
2023-02-02 10:07:48 +0700

    Filter data

--- Projecy.py
+++ Projecy.py
@@ -1,6 +1,21 @@
 # I Data Preparation
 # 1. Load Time Series Data
 
+from pandas import read_csv
+
+series = read_csv(r'D:\UEL\Nam_3\Hoc_ki_6\timeseries\Data\Taichinh.csv', header=0, index_col=0, parse_dates=True,
+squeeze=True)
+series.insert(0,'row_id',range(0, 0 + len(series)), allow_duplicates=False) # Add row_id column to dataset
+print(series.head())
+print(series.columns)
+print("Dataframe before filtering: ", len(series.index))
+
+code_need = ['E1VFVN30', 'FUESSV50', 'FUCTVGF3','FUCVREIT', 'HCM']
+series = series[series.code.isin(code_need)]
+print("Dataframe after filtering: ", len(series.index))
+
+
+
 # 2.  Exploring Time Series Data
 
 # II. Feature Engineering
